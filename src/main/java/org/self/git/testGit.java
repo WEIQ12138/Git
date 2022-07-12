39262975225eeb6a0f07363ded0811159246f28e
WEIQ <WEIQ@git.com>
2022-07-12 21:11:48 +0800

    first master

---
+++ src/main/java/org/self/git/testGit.java
@@ -0,0 +1,12 @@
+package org.self.git;
+
+/**
+ * @author
+ * @create 2022-07-12-21:01
+ */
+public class testGit {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
